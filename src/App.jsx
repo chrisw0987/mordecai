61b55543e733744dc5c262f3a22172728662484d
chrisw0987 <wuc0987@gmail.com>
2026-04-16 16:44:04 -0400

    Add review page with token + feedback logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ScrollToTop from './Components/ScrollToTop/ScrollToTop';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 import Home from './Pages/Home/Home';
@@ -9,13 +9,20 @@ import BookDemo from './Pages/BookDemo/BookDemo';
 import SMSConsent from './Pages/SMSConsent/SMSConsent';
 import PrivacyPolicy from './Pages/PrivacyPolicy/PrivacyPolicy';
 import TermsOfService from './Pages/TermsOfService/TermsOfService';
+import ReviewLanding from './Pages/ReviewLanding/ReviewLanding';
 import './App.css';
 
 function App() {
+  const location = useLocation()
+  const isReviewPage = location.pathname.startsWith('/review')
+
   return (
     <>
       <ScrollToTop />
-      <Navbar />
+
+      {/* Hide Navbar on review page */}
+      {!isReviewPage && <Navbar />}
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/book-demo" element={<BookDemo />} />
@@ -23,8 +30,13 @@ function App() {
         <Route path="/sms-consent" element={<SMSConsent />} />
         <Route path="/privacy" element={<PrivacyPolicy />} />
         <Route path="/terms" element={<TermsOfService />} />
+
+        {/* Review route */}
+        <Route path="/review/:token" element={<ReviewLanding />} />
       </Routes>
-      <Footer />
+
+      {/* Hide Footer on review page */}
+      {!isReviewPage && <Footer />}
     </>
   );
 }
